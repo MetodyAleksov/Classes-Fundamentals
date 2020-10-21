e5d28e5e3f8c63a424222df49bf2c80afdecc61c
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-21 21:12:05 +0300

    Add project files.

---
+++ Classes-Fundamentals/Dice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Fundamentals
+{
+    class Dice
+    {
+        public Dice(int sides)
+        { 
+            Sides = sides;
+        }
+        public int Sides { get; set; }
+
+        public string Type { get; set; }
+
+        public void Roll()
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(1, Sides + 1));
+        }
+    }
+}
---
+++ Classes-Fundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace Classes_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice d6 = new Dice(int.Parse(Console.ReadLine()));
+            d6.Roll();
+        }
+    }
+}
