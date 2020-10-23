dc01b162d4bb42a0195c14a30cab2c3a6b9eba4b
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-23 15:13:16 +0300

    updated

---
+++ 01.AdvertismentMessage/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace _01.AdvertismentMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> prases = new List<string> 
+            { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can’t live without this product."};
+            List<string> events = new List<string> 
+            { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            List<string> authors = new List<string> 
+            { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            List<string> cities = new List<string>
+            {"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Random rnd = new Random();
+                string prase = prases[rnd.Next(0, prases.Count)];
+                string even = events[rnd.Next(0, events.Count)];
+                string author = authors[rnd.Next(0, authors.Count)];
+                string city = authors[rnd.Next(0, cities.Count)];
+                Console.WriteLine($"{prase} {even} {author} – {city}.");
+            }
+
+        }
+    }
+}
---
+++ 01.CompanyRoster/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CompanyRoster
+{
+    class Roster
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                Roster employee = new Roster();
+
+            }
+            List<string> departments = new List<string>();
+        }
+    }
+}
