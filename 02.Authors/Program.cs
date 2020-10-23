88e8d3a6a3b71ed2c93e15e5a8a29d678427edf0
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-23 16:03:57 +0300

    updated

--- 02.Authors/Program.cs
+++ 02.Authors/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.Authors
 {
@@ -6,7 +9,49 @@ namespace _02.Authors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>(n);
+            for (int i = 0; i < n; i++)
+            {
+                string[] newartivle = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                Article article = new Article(newartivle[0], newartivle[1], newartivle[2]);
+                articles.Add(article);
+            }
+            string itemToTake = Console.ReadLine();
+            if (itemToTake == "title")
+            {
+                articles = articles.OrderBy(x => x.Title).ToList();
+            }
+            else if (itemToTake == "content")
+            {
+                articles = articles.OrderBy(x => x.Content).ToList();
+            }
+            else if (itemToTake == "author")
+            {
+                articles = articles.OrderBy(x => x.Author).ToList();
+            }
+            foreach (Article item in articles)
+            {
+                Console.WriteLine($"{item.Title} - {item.Content}: {item.Author}");
+            }
         }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
     }
 }
