1bc8f44580126280ddfaa05e6775e70022c7fda7
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-10-25 15:40:29 +0200

    updated

--- 01.CompanyRoster/Program.cs
+++ 01.CompanyRoster/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace _01.CompanyRoster
 {
     class Roster
     {
+        public Roster(string name, double salary, string department)
+        {
+            Name = name;
+            Salary = salary;
+            Department = department;
+        }
+
         public string Name { get; set; }
         public double Salary { get; set; }
         public string Department { get; set; }
@@ -14,16 +22,47 @@ namespace _01.CompanyRoster
     {
         static void Main(string[] args)
         {
+            List<Roster> roster = new List<Roster>();
             int n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
                 string[] input = Console.ReadLine()
                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                     .ToArray();
-                Roster employee = new Roster();
-
+                Roster employee = new Roster(input[0], double.Parse(input[1]), input[2]);
+                roster.Add(employee);
             }
-            List<string> departments = new List<string>();
+            double biggestAverage = double.MinValue;
+            string biggestDep = String.Empty;
+            foreach (Roster item in roster)
+            {
+                string currDepartment = item.Department;
+                double currAvrg = 0;
+                int depCounter = 0;
+                foreach (Roster employee in roster)
+                {
+                    if (employee.Department == currDepartment)
+                    {
+                        depCounter++;
+                        currAvrg += employee.Salary;
+                    }
+                }
+                currAvrg /= depCounter;
+                if (currAvrg > biggestAverage)
+                {
+                    biggestAverage = currAvrg;
+                    biggestDep = currDepartment;
+                }
+            }
+            Console.WriteLine($"Highest Average Salary: {biggestDep}");
+            List<Roster> biggestAvrg = roster.Where(x => x.Department == biggestDep).ToList();
+            biggestAvrg = biggestAvrg.OrderByDescending(x => x.Salary).ToList();
+            StringBuilder sb = new StringBuilder();
+            foreach (Roster item in biggestAvrg)
+            {
+                sb.AppendLine($"{item.Name} {item.Salary:f2}");
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
---
+++ 02.OldestFamilyMember/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OldestFamilyMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Member> family = new List<Member>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                Member currPerson = new Member(input[0], int.Parse(input[1]));
+                family.Add(currPerson);
+            }
+            family = family.OrderByDescending(x => x.Age).ToList();
+            Console.WriteLine(family[0].ToString());
+        }
+    }
+    class Member
+    {
+        public Member(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
